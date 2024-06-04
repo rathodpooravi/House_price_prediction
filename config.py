6a41a00d70896be5663dabcff27d2c7288722cf6
rathodpooravi <rathodpooravi.2@gmail.com>
2024-06-04 15:37:50 +0530

    files

--- config.py
+++ config.py
@@ -1,6 +1,9 @@
-MODEL_FILE_PATH = r'House_Linear.pkl'
-JSON_FILE_PATH = r'project_data.json'
+MODEL_FILE_PATH = r'F:\DataScience\Practice\Machine Learning\My_API\House_Linear.pkl'
+JSON_FILE_PATH = r'F:\DataScience\Practice\Machine Learning\My_API\project_data.json'
+
+
 
 
 
 PORT_NUMBER=5005
+
